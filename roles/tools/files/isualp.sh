56df32d798b2c1558e58283be680f70aa5cf40fa
Masaki.Nakano <mnakano@hpcs.cs.tsukuba.ac.jp>
2024-12-08 10:41:04 +0900

    fix measure script

--- roles/tools/files/isualp.sh
+++ roles/tools/files/isualp.sh
@@ -4,7 +4,7 @@ set -eux
 
 DISCORD_WEBHOOK="https://discord.com/api/webhooks/1314864227587981312/qv5PjNAh5xUiqvGIlUChpyBHK_8toi3tfNdm36L4clmd5Ss6ywywVMjWIFY0KoaML2L3"
 
-cat /var/log/nginx/access.log | alp ltsv -m \
+sudo cat /var/log/nginx/access.log | alp ltsv -m \
 "\
 /api/user/[^/]+/icon,\
 /api/user/[^/]+/theme,\
